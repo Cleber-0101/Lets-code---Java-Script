62aaa460c501c88c6730fe1106cb5c60c052843b
cleber <cleberbatista99@hotmail.com>
2022-06-26 18:05:11 -0300

    variaveis

--- logica.js
+++ logica.js
@@ -1,1 +1,9 @@
-alert("alerta")
+var a = 10;    //tipo inteiro 
+
+var b = 13.5;    //numero real 
+
+let c = "Cleber";   //tipo texto-sting (sequencia de caracteres) 
+
+let d = false ;   //buleana contem informaçoes que são verdadeira ou falsas 
+
+d = 15;
