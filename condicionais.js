e0a07b703fd914f1efa6a0d7c656d4fbf6c9bcfb
cleber <cleberbatista99@hotmail.com>
2022-06-26 21:00:59 -0300

    aula de condicionais

---
+++ condicionais.js
@@ -0,0 +1,13 @@
+// criando um programa que consegue tomar decisoes 
+let estaSol = false;
+
+//condicional ("  if significa se!")
+if(estaSol) {
+    console.log ('vou pra praia')
+}
+
+
+//condicional "else significa se ela é falsa "
+else {
+    console.log('vou pro campo');
+}
--- logica.js
+++ logica.js
@@ -1,9 +1,14 @@
-var a = 10;    //tipo inteiro 
+var a = 10;    //numero tipo inteiro 
 
-var b = 13.5;    //numero real 
+var b = 13.5;    //numero tipo real 
 
 let c = "Cleber";   //tipo texto-sting (sequencia de caracteres) 
+//varivel let faz com que a variavel não fuja do seu escopo 
 
 let d = false ;   //buleana contem informaçoes que são verdadeira ou falsas 
 
 d = 15;
+
+a = function () {} ;
+
+console.log(1)
